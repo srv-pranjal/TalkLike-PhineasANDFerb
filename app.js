8a8579ed3b5883470c19196f8d822412ca8ac4ad
pranjal.s <pranjal.s@highradius.com>
2021-08-25 00:48:00 +0530

    update: added error handler

--- app.js
+++ app.js
@@ -3,15 +3,20 @@ const btnTranslate = document.querySelector("#btn-translate");
 const resultArea = document.querySelector("#result");
 
 const apiURL = "https://api.funtranslations.com/translate/ferb-latin.json";
-
 function constructURL(text) {
   return `${apiURL}?text=${text}`;
+}
+
+function errorHandler(e) {
+  alert("Oops! The Server seems to be down. Please try after some time");
 }
 
 function clickHandler() {
   const inputText = textInput.value;
   fetch(constructURL(inputText))
     .then((response) => response.json())
-    .then((json) => (resultArea.innerText = json.contents.translated));
+    .then((json) => (resultArea.innerText = json.contents.translated))
+    .catch(errorHandler);
 }
 btnTranslate.addEventListener("click", clickHandler);
+textInput.addEventListener("click", () => (resultArea.innerHTML = ""));
